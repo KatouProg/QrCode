214c7ecf9a377b8dd3c021cd1d4b3a43a34fc30a
Quentin BUXO <katouprog@gmail.com>
2021-12-20 19:32:03 +0900

    Files upload

---
+++ dist/script.js
@@ -0,0 +1,18 @@
+const input = document.querySelectorAll("input");
+const canvas = document.getElementById("qr");
+
+const createQR = v => {
+  return new QRious({
+    element: canvas,
+    value: v,
+    size: 400,
+    backgroundAlpha: 0,
+    foreground: "white" });
+
+};
+
+const qr = createQR(input.value);
+
+input.addEventListener("input", () => {
+  const qr = createQR(input.value);
+});
